fbcd5dd0774b3ab1e1a0b65073a1469f72835481
Yusup <hunzah16@gmail.com>
2023-01-29 19:55:47 +0300

    Practice 3

--- JS-App-1.js
+++ JS-App-1.js
@@ -1,8 +1,12 @@
-let numberOfFilms;
-function start () {
+'use strict'
 
+let numberOfFilms;
+
+function start () {
 numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', ''); 
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms))
+   
+
+while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms))
 {numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', ''); } 
 }
 start ();
@@ -15,28 +19,28 @@ const personalMovieDB = {
     privat: false
 };
 
-// function rememberMyFilms (){
-// for (let i = 0; i < 2; i++) {
-//     const a = prompt('Один из последних просмотренных фильмов?', ''),
-//           b = +prompt('На сколько оцените его?', '');
+function rememberMyFilms (){
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', '').trim (),
+          b = +prompt('На сколько оцените его?', '');
 
-//         if (a != null && b !=null && a != '' && a.length<50) {
-//             personalMovieDB.movies [a]= b;
-//             console.log ('done');
-//         }
-//             else { console.log ('error'); 
-//         i--;}
-//     }
-// }
-// rememberMyFilms ();
+        if (a != null && b !=null && a != '' && a.length<50) {
+            personalMovieDB.movies [a]= b;
+            console.log ('done');
+        }
+            else { console.log ('error'); 
+        i--;}
+    }
+}
+rememberMyFilms ();
 
-// function detectPersonalLevel (){
-// if (personalMovieDB.count<=10) {console.log ('You a bitch ass');}
-// else if  (personalMovieDB.count>10 && personalMovieDB.count<=30 ) {console.log ('Вы классический Нигер!');}
-// else if  (personalMovieDB.count>30 ) {console.log ('Вы задрот!');}
-// else {console.log ('Произошла ошибка');}
-// }
-// detectPersonalLevel ();
+function detectPersonalLevel (){
+if (personalMovieDB.count<=10) {console.log ('You a bitch ass');}
+else if  (personalMovieDB.count>10 && personalMovieDB.count<=30 ) {console.log ('Вы классический Нигер!');}
+else if  (personalMovieDB.count>30 ) {console.log ('Вы задрот!');}
+else {console.log ('Произошла ошибка');}
+}
+detectPersonalLevel ();
 
 
 function showMyDB (hidden){
@@ -48,7 +52,7 @@ showMyDB(personalMovieDB.privat)
 function writeYourGenres () 
     {for (let i =1; i<=3; i++)
 
-    {const genre = prompt(`Ваш любимый жанр под ${i}`);
+    {const genre = prompt(`Ваш любимый жанр под ${i}`).trim ();
     personalMovieDB.genres [i-1] = genre;
     }
 }
